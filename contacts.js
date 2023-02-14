a0e6b6cbe28413819fff9bcdab52de3e5d51679e
Adilnurlan <nurlan.adil98@gmail.com>
2023-02-14 23:52:13 +0600

    third

--- contacts.js
+++ contacts.js
@@ -17,38 +17,50 @@ async function listContacts() {
 // listContacts();
 
 async function getContactById(contactId) {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const result = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const result = JSON.parse(data);
 
-  const found = result.find((contact) => contact.id === contactId);
-  console.table(found);
+    const found = result.find((contact) => contact.id === contactId);
+    console.table(found);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // getContactById(5);
 
 async function removeContact(contactId) {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const result = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const result = JSON.parse(data);
 
-  const deleteContact = result.filter((contact) => contactId !== contact.id);
-  console.table(deleteContact);
+    const deleteContact = result.filter((contact) => contactId !== contact.id);
+    console.table(deleteContact);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // removeContact(5);
 
 async function addContact(name, email, phone) {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const result = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const result = JSON.parse(data);
 
-  const newContact = {
-    id: uuidv4(),
-    name,
-    email,
-    phone,
-  };
-  result.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(result), "utf-8");
-  console.table(result);
+    const newContact = {
+      id: uuidv4(),
+      name,
+      email,
+      phone,
+    };
+    result.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(result), "utf-8");
+    console.table(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // addContact("adil", "dafsfdf@fdsf.com", "44544545454");
